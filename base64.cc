83fe2de0df2f8d30aee0a5807967f76e10a2805d
qwertybomb <42456119+qwertybomb@users.noreply.github.com>
2020-06-27 15:15:30 -0400

    Add files via upload

--- base64.cc
+++ base64.cc
@@ -15,7 +15,7 @@ namespace base64
 		template <std::size_t S>
 		constexpr FixedString(FixedString<S> const &other)
 		{
-			for (std::size_t i = 0; i != other.size(); ++i)
+			for (std::size_t i = (S > N ? N : S); i--;)
 				buf[i] = other.buf[i];
 		}
 		constexpr operator char const *() const { return buf; }
@@ -30,11 +30,12 @@ namespace base64
 	auto constexpr decode()
 	{
 		std::size_t constexpr string_size = string.size();
-		auto constexpr find_padding = [string_size](){ 
+		auto constexpr find_padding = [string_size]() {
 			std::size_t i;
-			for(i=0; i < string_size; ++i)
-				if(string[i] == '=') break;
-			 return i;
+			for (i = 0; i < string_size; ++i)
+				if (string[i] == '=')
+					break;
+			return i;
 		};
 		FixedString<(string_size * 3) / 4> result;
 		auto constexpr convert_char = [](auto const &ch) {
@@ -67,9 +68,9 @@ namespace base64
 	{
 		std::size_t constexpr string_size = string.size();
 		std::size_t constexpr result_size_no_padding = (string_size * 4 + 2) / 3;
-		std::size_t constexpr result_size = (result_size_no_padding+3)&(-4);
+		std::size_t constexpr result_size = (result_size_no_padding + 3) & (-4);
 		std::size_t constexpr padding_size = result_size - result_size_no_padding;
-		FixedString<(string_size+3)&(-4)> string_with_padding = string;
+		FixedString<(string_size + 3) & (-4)> string_with_padding = string;
 		FixedString<result_size> result;
 		auto constexpr convert_num = [](auto const &num) {
 			if (num < 26)
@@ -93,8 +94,8 @@ namespace base64
 			result[j + 2] = convert_num(bytes[2]);
 			result[j + 3] = convert_num(bytes[3]);
 		}
-			for (std::size_t i = 0; i < padding_size; ++i)
-				result[result_size_no_padding + i] = '=';
+		for (std::size_t i = 0; i < padding_size; ++i)
+			result[result_size_no_padding + i] = '=';
 		return result;
 	}
 } // namespace base64
@@ -104,8 +105,9 @@ int main()
 
 	auto constexpr test = base64::encode<"Man is distinguished, not only by his reason, but by this singular passion from other animals, \
 which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable \
-generation of knowledge, exceeds the short vehemence of any carnal pleasure../..">();
+generation of knowledge, exceeds the short vehemence of any carnal pleasure.">();
 
 	std::cout << test << '\n';
-	std::cout << '\n' << base64::decode<test>() << '\n';
+	std::cout << '\n'
+			  << base64::decode<test>() << '\n';
 }
