18080372e4cf60cc96093ddccc2b97fbff25ef41
qwertybomb <42456119+qwertybomb@users.noreply.github.com>
2020-07-30 13:29:13 -0400

    Add files via upload

--- base64.cc
+++ base64.cc
@@ -9,45 +9,38 @@ struct FixedString
 	constexpr FixedString() = default;
 	constexpr FixedString(char const *s)
 	{
-		for (std::size_t i = 0; i != N; ++i)
-			buf[i] = s[i];
+		std::copy(s, s + N, buf);
 	}
+
 	template <std::size_t S>
 	constexpr FixedString(FixedString<S> const &other)
 	{
-		for (std::size_t i = (S > N ? N : S); i--;)
-			buf[i] = other.buf[i];
+		std::copy(other.buf, other.buf + std::min(S, N), buf);
 	}
+
 	auto constexpr operator == (FixedString const &other) const
 	{
-		for(std::size_t i = 0; i < N; ++i)
-		{
-			if(buf[i]!=other[i])
-				return false;
-		}
-		return true;
+		return std::equal(buf, buf + N, other.buf);
 	}
-	constexpr operator char const *() const { return buf; }
-	constexpr operator char *() { return buf; }
+
+	auto constexpr operator[](std::size_t index) const { return buf[index]; }
+	auto constexpr &operator[](std::size_t index) { return buf[index]; }
 	std::size_t constexpr size() const { return N; }
+
+	friend auto &operator<<(std::ostream& stream, const FixedString& string)
+	{
+		stream << string.buf;
+		return stream;
+	}
 };
 
 template <std::size_t N>
 FixedString(char const (&)[N]) -> FixedString < N - 1 >;
 
-template <FixedString string>
-auto constexpr decode()
+namespace detail
 {
-	std::size_t constexpr string_size = string.size();
-	auto constexpr find_padding = [string_size]() {
-		std::size_t i;
-		for (i = 0; i < string_size; ++i)
-			if (string[i] == '=')
-				break;
-		return i;
-	};
-	FixedString <find_padding() * 3 / 4 > result;
-	auto constexpr convert_char = [](auto const & ch) {
+auto constexpr convert_char (char const ch) 
+{
 		if (ch >= 'A' && ch <= 'Z')
 			return ch - 65;
 		else if (ch >= 'a' && ch <= 'z')
@@ -56,16 +49,39 @@ auto constexpr decode()
 			return ch + 4;
 		else
 			return ch == '+' ? 62 : 63;
+};
+
+auto constexpr convert_number (char const num)
+{
+		if (num < 26)
+			return static_cast<char>(num + 65);
+		else if (num > 25 && num < 52)
+			return static_cast<char>(num + 71);
+		else if (num > 51)
+			return static_cast<char>(num - 4);
+		else
+			return num == 62 ? '+' : '/';
+};
+}
+
+template <FixedString string>
+auto constexpr decode()
+{
+	std::size_t constexpr string_size = string.size();
+	auto constexpr find_padding = [string_size]() {
+		return std::distance(string.buf, 
+               std::find(string.buf, string.buf + string_size, '='));
 	};
+	FixedString <find_padding() * 3 / 4 > result;
 	for (std::size_t i = 0, j = 0; i < string_size; i += 4, j += 3)
 	{
 		char bytes[3] = {
-			static_cast<char>(convert_char(string[i]) << 2
-			| convert_char(string[i + 1]) >> 4),
-			static_cast<char>(convert_char(string[i + 1]) << 4
-			| convert_char(string[i + 2]) >> 2),
-			static_cast<char>(convert_char(string[i + 2]) << 6
-			| convert_char(string[i + 3])),
+			static_cast<char>(detail::convert_char(string[i]) << 2
+			| detail::convert_char(string[i + 1]) >> 4),
+			static_cast<char>(detail::convert_char(string[i + 1]) << 4
+			| detail::convert_char(string[i + 2]) >> 2),
+			static_cast<char>(detail::convert_char(string[i + 2]) << 6
+			| detail::convert_char(string[i + 3])),
 		};
 		result[j] = bytes[0];
 		result[j + 1] = bytes[1];
@@ -84,16 +100,6 @@ auto constexpr encode()
 	std::size_t constexpr padding_size = result_size - result_size_no_padding;
 	FixedString <(string_size + 2) / 3 * 3> constexpr string_with_padding = string;
 	FixedString<result_size> result;
-	auto constexpr convert_num = [](auto const & num) {
-		if (num < 26)
-			return static_cast<char>(num + 65);
-		else if (num > 25 && num < 52)
-			return static_cast<char>(num + 71);
-		else if (num > 51)
-			return static_cast<char>(num - 4);
-		else
-			return num == 62 ? '+' : '/';
-	};
 	for (std::size_t i = 0, j = 0; i < string_size; i += 3, j += 4)
 	{
 		/* convert every 3 bytes to 4 6 bit numbers
@@ -109,13 +115,10 @@ auto constexpr encode()
 			| string_with_padding[i + 2] >> 6),
 			static_cast<char>(string_with_padding[i + 2] & 63)
 		};
-		result[j] = convert_num(bytes[0]);
-		result[j + 1] = convert_num(bytes[1]);
-		result[j + 2] = convert_num(bytes[2]);
-		result[j + 3] = convert_num(bytes[3]);
+		std::transform(bytes, bytes + 4, result.buf + j, detail::convert_number);
 	}
-	for (std::size_t i = 0; i < padding_size; ++i)
-		result[result_size_no_padding + i] = '=';
+	std::fill_n(result.buf + result_size_no_padding, padding_size, '=');
+
 	return result;
 }
 } // namespace base64
@@ -126,7 +129,7 @@ int main()
 
 	auto constexpr input_text = base64::FixedString { "Man is distinguished, not only by his reason, but by this singular passion from other animals, \
 which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable \
-generation of knowledge, exceeds the short vehemence of any carnal pleasure."};
+generation of knowledge, exceeds the short vehemence of any carnal pleasure." };
 	auto constexpr encoded = base64::encode<input_text>();
 	auto constexpr decoded = base64::decode<encoded>();
 	static_assert(decoded == input_text);
